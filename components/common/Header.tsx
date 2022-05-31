0f5d06622b7226cd659054b2460ae01fc130130b
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-05-31 12:30:33 +0300

    created links

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import { FC } from 'react'
+import Link from 'next/link'
 
-const Header = () => {
+const Header: FC = () => {
     return <nav className="navbar navbar-expand-lg navbar-light bg-light container">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Auction</a>
+            <Link passHref={true} href='/'>
+                <a className="navbar-brand" href="#">Auction</a>
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Sign In</a>
-                    </li>
+                    <Link passHref={true} href='/'>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                    </Link>
+                    <Link passHref={true} href='/sign-in'>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Sign In</a>
+                        </li>
+                    </Link>
                 </ul>
             </div>
         </div>
