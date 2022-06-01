26aa9c3d85c8b6d850b71b7cfd99fa9fe57572c8
Akifcan Kara <48747986+Akifcan@users.noreply.github.com>
2022-06-01 14:34:32 +0300

    listed single product

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react'
 import styles from '@/styles/product.module.css'
 import { RiAuctionLine } from 'react-icons/ri'
 import { ProductProps } from '@/helpers/prototypes'
+import Link from 'next/link'
 
 interface ProductCardProps {
     height?: number,
@@ -10,17 +11,19 @@ interface ProductCardProps {
 }
 
 const ProductCard: FC<ProductCardProps> = ({ height = 200, product }) => {
-    return <div aria-label='Product Card' className={`${styles.product} shadow  bg-body rounded`}>
-        <div className={styles.image} style={{ height }}>
-            <img src={product.imageUrl} alt={product.name} />
+    return <Link passHref={true} href={`/product/${product.id}`}>
+        <div aria-label='Product Card' className={`${styles.product} shadow  bg-body rounded`} style={{ cursor: 'pointer' }}>
+            <div className={styles.image} style={{ height }}>
+                <img src={product.imageUrl} alt={product.name} />
+            </div>
+            <div className={`p-2`}>
+                <h5>{product.name}</h5>
+                <p className='text-muted'>{product.description}</p>
+                <p>Start Price: <b>{product.price}$</b> </p>
+            </div>
+            <button type="button" className="btn btn-primary w-100 rounded-0">Bid Now <RiAuctionLine fontSize={'1.5rem'} /></button>
         </div>
-        <div className={`p-2`}>
-            <h5>{product.name}</h5>
-            <p className='text-muted'>{product.description}</p>
-            <p>Start Price: <b>{product.price}$</b> </p>
-        </div>
-        <button type="button" className="btn btn-primary w-100 rounded-0">Bid Now <RiAuctionLine fontSize={'1.5rem'} /></button>
-    </div>
+    </Link>
 }
 
 export default ProductCard
--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,35 +1,44 @@
-import { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
 import Container from '@/components/common/Container'
 import ProductCard from '@/components/product/ProductCard'
 import AuctionLog from '@/components/product/AuctionLog'
 import { useRouter } from 'next/router'
+import { ProductProps } from '@/helpers/prototypes'
+import '@/helpers/prototypes'
+
 
 const ProductDetail: FC = () => {
 
     const router = useRouter()
+    const [product, setProduct] = useState<ProductProps>()
 
     useEffect(() => {
         if (!router.query.slug) return
-        console.log(router.query.slug);
-
+        fetch(`/product/${router.query.slug}`.apiRequest())
+            .then(res => res.json())
+            .then((data: ProductProps) => {
+                setProduct(data)
+            })
     }, [router])
 
-    return <Container navigation={[{ label: 'Home', href: '/' }, { label: 'Product Name' }]}>
-        <div className='row'>
-            <div className='col-md-6'>
-                {/* <ProductCard height={500} /> */}
-            </div>
-            <div className='col-md-6'>
-                <div className="alert alert-primary" role="alert">
-                    This Auction Not Start Yet
+    return <Container navigation={[{ label: 'Home', href: '/' }, { label: product ? product.name : 'Loading' }]}>
+        {product && (
+            <div className='row'>
+                <div className='col-md-6'>
+                    <ProductCard product={product} height={500} />
                 </div>
-                <div className="alert alert-light pb-0" role="alert">
-                    <p>Start Date: <time className='fw-bold'>31/05/2022</time></p>
-                    <p>End Date: <time className='fw-bold'>31/06/2022</time></p>
+                <div className='col-md-6'>
+                    <div className="alert alert-primary" role="alert">
+                        This Auction Not Start Yet
+                    </div>
+                    <div className="alert alert-light pb-0" role="alert">
+                        <p>Start Date: <time className='fw-bold'>31/05/2022</time></p>
+                        <p>End Date: <time className='fw-bold'>31/06/2022</time></p>
+                    </div>
+                    <AuctionLog />
                 </div>
-                <AuctionLog />
             </div>
-        </div>
+        )}
     </Container>
 }
 
